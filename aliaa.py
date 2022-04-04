31c481f351063e0f7608d7775a49709f76431585
Mostafa Ali <ma7952057@gmail.com>
2022-04-04 12:54:27 +0200

    initial commit

---
+++ aliaa.py
@@ -0,0 +1,62 @@
+num_of_day = int(input("ممكن تدخلي رقم يوم النهارده و معلش لو يكون بالانجلش يا قلبي :"))
+
+if num_of_day == 1:
+    print ("انا بحبك ")
+elif num_of_day == 2:
+    print ("انا بعشفك")
+elif num_of_day == 3:
+    print ("ربنا يخليكي ليا وميحرمنيش منك ابدا")
+elif num_of_day == 5:
+    print ("انتي جياتي كلها")
+elif num_of_day == 6:
+    print ("انتي قلبي من جوه")
+elif num_of_day == 7:
+    print ("ستظلين دايما في الركن الدافئ في قلبي")
+elif num_of_day == 8:
+    print ("لن تبارحي قلبي ابدا ما بقيتي")
+elif num_of_day == 9:
+    print ("احبك يا اجمل سبب لكل شئ جميل في حياتي ")
+elif num_of_day == 10:
+    print ("ستبقي دائما الجظء الاجمل في قلبي")
+elif num_of_day == 11:
+    print ("في طريقي الف شخص و في عيني انتي فقط")
+elif num_of_day == 12:
+    print ("لست امام عيني ولكني لا اري غيرك")
+elif num_of_day == 13:
+    print ("انتي اللحظه الحلوه في كل يوم جميل في حياتي ")
+elif num_of_day == 14:
+    print ("عساني اسر قلبك ولا اضره ابدا")
+elif num_of_day == 15:
+    print ("انتي كلي وجميعي ومعظمي وكافتي")  
+elif num_of_day == 16:
+    print ("ستبقي دائما شئ مختلف عن الجميع")
+elif num_of_day == 17:
+    print ("يا اجمل حاجه في حياتي انتي")
+elif num_of_day == 18:
+    print ("في ملامح وجهك الحلو كل الامان")
+elif num_of_day == 19:
+    print ("اني اتمناكي حبا لا يشوبه اي فقد")
+elif num_of_day == 20:
+    print ("كل نواحي قلبي مغرمه بك")
+elif num_of_day == 21:
+    print ("وجودك يجعل كل شئ بخير حتي انا")
+elif num_of_day == 22:
+    print ("تبارك الله ما احلاكي في نظري")
+elif num_of_day == 23:
+    print ("خلق الجمال لتختصره عيناكي")
+elif num_of_day == 24:
+    print ("ليت كل الهواء رائحتك الجميله")
+elif num_of_day == 25:
+    print ("في بسمتك تجتمع تفاصيل الجمال كلها")  
+elif num_of_day == 26:
+    print ("والله اني اراكي في قلب عيني اجمل جنه")  
+elif num_of_day == 27:
+    print ("ارفض ربيع الغير واحب خريفك انتي")      
+elif num_of_day == 28:
+    print ("باختصار انتي اكتفائي")  
+elif num_of_day == 29:
+    print ("عيناكي تجيد ارباكي")  
+elif num_of_day == 30:
+    print ("كل شئ معك مختلف حتي ضحكتي")  
+elif num_of_day == 31:
+    print ("انا بحمد ربنا عليكي دايما وان شاء الله تكوني من نصيبي طول العمر")
